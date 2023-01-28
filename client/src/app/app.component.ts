c8fadcd244dcbd2d3e0d89fa0381219cce9f2f21
Freddy Marin <freddymarin.jpg@gmail.com>
2023-01-28 08:43:58 -0600

    added getAll from api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   
 })
 export class AppComponent {
-  title = 'client';
-}
+  title = 'Vehicles';
+  vehicles: any; // property to store the fetched data
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:5000/api/vehicles')
+      .subscribe(data => {
+        this.vehicles = data;
+        console.log(this.vehicles);
+      });
+  }}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
