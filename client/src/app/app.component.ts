c5ae6d968378de88dfdf61548eceb3d33c5a2695
Freddy Marin <freddymarin.jpg@gmail.com>
2023-01-28 09:36:07 -0600

    added CRD functionalities

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,10 +13,29 @@ export class AppComponent {
 
   constructor(private http: HttpClient) { }
 
+  deleteVehicle(placa: string) {
+    this.http.delete('http://localhost:5000/api/vehicles/' + placa).subscribe(data=>{
+      console.log("deleted: " + placa + '/' + data)
+      location.reload()
+    })
+  }
+
+  addVehicle(placa: string, marca: string, modelo: string, serie: string, color: string){
+    this.http.post('http://localhost:5000/api/vehicles/', { placa, marca, modelo, serie, color })
+      .subscribe(data => {
+        console.log(data);
+        location.reload()
+      });
+  }
+
   ngOnInit() {
     this.http.get('http://localhost:5000/api/vehicles')
       .subscribe(data => {
         this.vehicles = data;
         console.log(this.vehicles);
       });
-  }}
+      
+  }
+}
+
+  
